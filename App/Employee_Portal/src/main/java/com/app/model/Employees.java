4a2098aa911d1f9a936226317a86f7ffd68eb74d
Michael Pearson <mpearson2516@yahoo.com>
2021-02-12 13:35:18 -0800

    Created the maven project and the Employees model class

---
+++ App/Employee_Portal/src/main/java/com/app/model/Employees.java
@@ -0,0 +1,210 @@
+package com.app.model;
+
+import java.sql.Date;
+
+public class Employees {
+
+	private int employeeNum;
+	private String firstName;
+	private String lastName;
+	private String email;
+	private long phone;
+	private String gender;
+	private Date dob;
+	private long ssn;
+	private double salary;
+	private String title;
+	private String password;
+	
+	public Employees() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public Employees(int employeeNum, String firstName, String lastName, String email, long phone, String gender,
+			Date dob, long ssn, double salary, String title, String password) {
+		super();
+		this.employeeNum = employeeNum;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+		this.phone = phone;
+		this.gender = gender;
+		this.dob = dob;
+		this.ssn = ssn;
+		this.salary = salary;
+		this.title = title;
+		this.password = password;
+	}
+
+	public int getEmployeeNum() {
+		return employeeNum;
+	}
+
+	public void setEmployeeNum(int employeeNum) {
+		this.employeeNum = employeeNum;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public long getPhone() {
+		return phone;
+	}
+
+	public void setPhone(long phone) {
+		this.phone = phone;
+	}
+
+	public String getGender() {
+		return gender;
+	}
+
+	public void setGender(String gender) {
+		this.gender = gender;
+	}
+
+	public Date getDob() {
+		return dob;
+	}
+
+	public void setDob(Date dob) {
+		this.dob = dob;
+	}
+
+	public long getSsn() {
+		return ssn;
+	}
+
+	public void setSsn(long ssn) {
+		this.ssn = ssn;
+	}
+
+	public double getSalary() {
+		return salary;
+	}
+
+	public void setSalary(double salary) {
+		this.salary = salary;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((dob == null) ? 0 : dob.hashCode());
+		result = prime * result + ((email == null) ? 0 : email.hashCode());
+		result = prime * result + employeeNum;
+		result = prime * result + ((firstName == null) ? 0 : firstName.hashCode());
+		result = prime * result + ((gender == null) ? 0 : gender.hashCode());
+		result = prime * result + ((lastName == null) ? 0 : lastName.hashCode());
+		result = prime * result + ((password == null) ? 0 : password.hashCode());
+		result = prime * result + (int) (phone ^ (phone >>> 32));
+		long temp;
+		temp = Double.doubleToLongBits(salary);
+		result = prime * result + (int) (temp ^ (temp >>> 32));
+		result = prime * result + (int) (ssn ^ (ssn >>> 32));
+		result = prime * result + ((title == null) ? 0 : title.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Employees other = (Employees) obj;
+		if (dob == null) {
+			if (other.dob != null)
+				return false;
+		} else if (!dob.equals(other.dob))
+			return false;
+		if (email == null) {
+			if (other.email != null)
+				return false;
+		} else if (!email.equals(other.email))
+			return false;
+		if (employeeNum != other.employeeNum)
+			return false;
+		if (firstName == null) {
+			if (other.firstName != null)
+				return false;
+		} else if (!firstName.equals(other.firstName))
+			return false;
+		if (gender == null) {
+			if (other.gender != null)
+				return false;
+		} else if (!gender.equals(other.gender))
+			return false;
+		if (lastName == null) {
+			if (other.lastName != null)
+				return false;
+		} else if (!lastName.equals(other.lastName))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		if (phone != other.phone)
+			return false;
+		if (Double.doubleToLongBits(salary) != Double.doubleToLongBits(other.salary))
+			return false;
+		if (ssn != other.ssn)
+			return false;
+		if (title == null) {
+			if (other.title != null)
+				return false;
+		} else if (!title.equals(other.title))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		return "Employees [employeeNum=" + employeeNum + ", firstName=" + firstName + ", lastName=" + lastName
+				+ ", email=" + email + ", phone=" + phone + ", gender=" + gender + ", dob=" + dob + ", ssn=" + ssn
+				+ ", salary=" + salary + ", title=" + title + ", password=" + password + "]";
+	}
+	
+}
