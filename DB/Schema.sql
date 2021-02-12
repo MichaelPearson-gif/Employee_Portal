530719e0462a2d68d92ba85304aa0206874a819b
Michael Pearson <mpearson2516@yahoo.com>
2021-02-12 13:22:08 -0800

    Edited schema a bit

--- DB/Schema.sql
+++ DB/Schema.sql
@@ -1,44 +1,39 @@
-CREATE TABLE "employees" (
+CREATE TABLE employees (
     "emp_no" SERIAL PRIMARY KEY,
-    "first_name" CHAR   NOT NULL,
-    "last_name" CHAR   NOT NULL,
-    "email" CHAR   NOT NULL,
-    "phone" NUMERIC   NOT NULL,
-    "gender" CHAR   NOT NULL,
-    "birthday" DATE   NOT NULL,
-    "ssn" INT   NOT NULL,
-    "salary" NUMERIC   NOT NULL,
-    "title" CHAR   NOT NULL,
-    "password" CHAR   NOT NULL
+    "first_name" CHAR(15) NOT NULL,
+    "last_name" CHAR(15) NOT NULL,
+    "email" CHAR(30) NOT NULL,
+    "phone" NUMERIC NOT NULL,
+    "gender" CHAR(1) NOT NULL,
+    "birthday" DATE NOT NULL,
+    "ssn" INT NOT NULL,
+    "salary" NUMERIC NOT NULL,
+    "title" CHAR(10) NOT NULL,
+    "password" CHAR(20) NOT NULL
 );
 
-CREATE TABLE "managers" (
+CREATE TABLE managers (
     "manager_no" SERIAL PRIMARY KEY,
-    "emp_no" INT NOT NULL
+    "emp_no" INT NOT NULL REFERENCES employees(emp_no)
 );
 
-CREATE TABLE "employee_manager" (
+CREATE TABLE employee_manager (
     "emp_man" SERIAL PRIMARY KEY,
-    "manager_no" INT NOT NULL,
-    "emp_no" INT NOT NULL
+    "manager_no" INT NOT NULL REFERENCES managers(manager_no),
+    "emp_no" INT NOT NULL REFERENCES employees(emp_no)
 );
 
-CREATE TABLE "requests" (
+CREATE TABLE requests (
     "request_no" SERIAL PRIMARY KEY,
-    "emp_no" INT   NOT NULL,
-    "amount" NUMERIC   NOT NULL,
-    "reciept" CHAR   NOT NULL,
-    "status" CHAR NOT NULL
+    "emp_no" INT NOT NULL REFERENCES employees(emp_no),
+    "amount" NUMERIC NOT NULL,
+    "reciept" CHAR NOT NULL,
+    "status" CHAR(10) NOT NULL
 );
 
-ALTER TABLE "managers" ADD CONSTRAINT "fk_managers_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "employee_manager" ADD CONSTRAINT "fk_employee_manager_manager_no" FOREIGN KEY("manager_no")
-REFERENCES "managers" ("manager_no");
-
-ALTER TABLE "employee_manager" ADD CONSTRAINT "fk_employee_manager_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
-
-ALTER TABLE "requests" ADD CONSTRAINT "fk_requests_emp_no" FOREIGN KEY("emp_no")
-REFERENCES "employees" ("emp_no");
+-- Alter the sequences of the serial id's
+ALTER SEQUENCE employees_emp_no_seq RESTART WITH 1000 INCREMENT BY 1;
+ALTER SEQUENCE managers_manager_no_seq RESTART WITH 2000 INCREMENT BY 1;
+ALTER SEQUENCE employee_manager_emp_man_seq RESTART WITH 3000 INCREMENT BY 1;
+-- Only need to restart the requests id sequence in case I need to restart from 1
+ALTER SEQUENCE requests_request_no_seq RESTART WITH 1 INCREMENT BY 1;
