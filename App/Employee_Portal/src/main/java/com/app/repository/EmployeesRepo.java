957dba0b21b3119819575059c0e24644452b7496
Michael Pearson <mpearson2516@yahoo.com>
2021-02-15 12:19:11 -0800

    Created the EmployeesRepo interface

---
+++ App/Employee_Portal/src/main/java/com/app/repository/EmployeesRepo.java
@@ -0,0 +1,23 @@
+package com.app.repository;
+
+import java.util.List;
+
+import com.app.model.Employees;
+
+public interface EmployeesRepo {
+
+	// Employee can view their info
+	Employees getInfo(int employeeId);
+	
+	// Get Employee email & password to verify login credentials
+	
+	// Get a list of employee emails to verify that it is in the system
+	List<String> allEmails();
+	
+	// Get the password associated with the employee email
+	String getPassword(String email);
+	
+	// Employee updates their info
+	Employees updateInfo(Employees employee);
+	
+}
