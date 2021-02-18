e811bf6ee89c2456d7777130a77e9eb748283e56
Michael Pearson <mpearson2516@yahoo.com>
2021-02-18 13:32:40 -0800

    Edited employees table in the schema

--- DB/Schema.sql
+++ DB/Schema.sql
@@ -1,8 +1,7 @@
 CREATE TABLE employees (
-    emp_id SERIAL PRIMARY KEY,
+    email CHAR(30) PRIMARY KEY,
     first_name CHAR(15) NOT NULL,
     last_name CHAR(15) NOT NULL,
-    email CHAR(30) NOT NULL,
     phone NUMERIC NOT NULL,
     gender CHAR(1) NOT NULL,
     birthday DATE NOT NULL,
@@ -14,18 +13,18 @@ CREATE TABLE employees (
 
 CREATE TABLE managers (
     manager_id SERIAL PRIMARY KEY,
-    emp_id INT NOT NULL REFERENCES employees(emp_id)
+    email CHAR(30) NOT NULL REFERENCES employees(email)
 );
 
 CREATE TABLE employee_manager (
     emp_man_id SERIAL PRIMARY KEY,
     manager_id INT NOT NULL REFERENCES managers(manager_id),
-    emp_id INT NOT NULL REFERENCES employees(emp_id)
+    email CHAR(30) NOT NULL REFERENCES employees(email)
 );
 
 CREATE TABLE requests (
     request_id SERIAL PRIMARY KEY,
-    emp_id INT NOT NULL REFERENCES employees(emp_id),
+    email CHAR(30) NOT NULL REFERENCES employees(email),
     manager_id INT REFERENCES managers(manager_id),
     amount NUMERIC NOT NULL,
     reciept CHAR NOT NULL,
@@ -34,9 +33,8 @@ CREATE TABLE requests (
 );
 
 -- Alter the sequences of the serial id's
-ALTER SEQUENCE employees_emp_id_seq RESTART WITH 1000 INCREMENT BY 1;
-ALTER SEQUENCE managers_manager_id_seq RESTART WITH 2000 INCREMENT BY 1;
-ALTER SEQUENCE employee_manager_emp_man_id_seq RESTART WITH 3000 INCREMENT BY 1;
+ALTER SEQUENCE managers_manager_id_seq RESTART WITH 100 INCREMENT BY 1;
+ALTER SEQUENCE employee_manager_emp_man_id_seq RESTART WITH 1000 INCREMENT BY 1;
 -- Only need to restart the requests id sequence in case I need to restart from 1
 ALTER SEQUENCE requests_request_id_seq RESTART WITH 1 INCREMENT BY 1;
 
