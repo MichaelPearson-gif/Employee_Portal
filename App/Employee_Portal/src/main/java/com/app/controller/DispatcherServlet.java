5ac604acc8766b0b21e09a3e755c7e91028e0cee
Michael Pearson <mpearson2516@yahoo.com>
2021-02-18 17:23:13 -0800

    Started creating Servlets and mapping them

---
+++ App/Employee_Portal/src/main/java/com/app/controller/DispatcherServlet.java
@@ -0,0 +1,22 @@
+package com.app.controller;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class DispatcherServlet extends HttpServlet{
+
+	// Servlet for my GET requests
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		
+	}
+	
+	// Servlet for my POST requests
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException{
+		
+	}
+	
+}
