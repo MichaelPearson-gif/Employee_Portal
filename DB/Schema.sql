f894c0dafda2e3865dba40db8868130cc85d4153
Michael Pearson <mpearson2516@yahoo.com>
2021-02-23 11:21:12 -0800

    Changed schema a bit and deleted the EmployeesToManager class

--- DB/Schema.sql
+++ DB/Schema.sql
@@ -17,9 +17,9 @@ CREATE TABLE managers (
 );
 
 CREATE TABLE employee_manager (
-    emp_man_id SERIAL PRIMARY KEY,
     manager_id INT NOT NULL REFERENCES managers(manager_id),
-    email CHAR(30) NOT NULL REFERENCES employees(email)
+    email CHAR(30) NOT NULL REFERENCES employees(email),
+    CONSTRAINT employee_manager_pk PRIMARY KEY(manager_id, email)
 );
 
 CREATE TABLE requests (
@@ -34,7 +34,6 @@ CREATE TABLE requests (
 
 -- Alter the sequences of the serial id's
 ALTER SEQUENCE managers_manager_id_seq RESTART WITH 100 INCREMENT BY 1;
-ALTER SEQUENCE employee_manager_emp_man_id_seq RESTART WITH 1000 INCREMENT BY 1;
 -- Only need to restart the requests id sequence in case I need to restart from 1
 ALTER SEQUENCE requests_request_id_seq RESTART WITH 1 INCREMENT BY 1;
 
