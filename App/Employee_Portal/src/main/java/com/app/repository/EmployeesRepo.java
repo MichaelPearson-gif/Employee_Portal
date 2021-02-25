90ca093d4eb87026bc9ee88a0bea7d9a8433dc84
Michael Pearson <mpearson2516@yahoo.com>
2021-02-25 09:24:21 -0800

    Changing the EmployeesRepo interface and implementation

--- App/Employee_Portal/src/main/java/com/app/repository/EmployeesRepo.java
+++ App/Employee_Portal/src/main/java/com/app/repository/EmployeesRepo.java
@@ -8,20 +8,11 @@ import com.app.exceptions.InvalidLoginException;
 import com.app.model.Employees;
 
 public interface EmployeesRepo {
-
-	// Employee can view their info
-	Employees getInfo(String email) throws BusinessException;
-	
-	// Get the password associated with the employee email
-	String getPassword(String email) throws InvalidLoginException;
 	
 	// Employee updates their info
 	void updateInfo(Employees employee) throws BusinessException;
 	
 	// List of all employees
 	List<String> employeeList() throws EmptyListException;
-	
-	// List of all the managers
-	List<String> managerList() throws EmptyListException;
 	
 }
