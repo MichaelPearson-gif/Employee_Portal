dc3d886e9edf5dd8a8e648d208812b15e7615e48
Michael Pearson <mpearson2516@yahoo.com>
2021-02-25 19:19:09 -0800

    Created implementation for the validate method

--- App/Employee_Portal/src/main/java/com/app/service/impl/EmployeesServiceImpl.java
+++ App/Employee_Portal/src/main/java/com/app/service/impl/EmployeesServiceImpl.java
@@ -2,16 +2,47 @@ package com.app.service.impl;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.app.exceptions.BusinessException;
 import com.app.model.Employees;
+import com.app.repository.EmployeesRepo;
+import com.app.repository.impl.EmployeesRepoImpl;
 import com.app.service.EmployeesService;
 
 public class EmployeesServiceImpl implements EmployeesService {
+	
+	// Instance of Repository layer implementation class
+	private EmployeesRepo employeesRepo = new EmployeesRepoImpl();
+	
+	// Logger variable
+	Logger log = Logger.getLogger(EmployeesServiceImpl.class);
 
 	@Override
 	public boolean validate(String email, String password) throws BusinessException {
-		// TODO Auto-generated method stub
-		return false;
+		
+		// Set default variable to false
+		boolean b = false;
+		
+		// Retrieve the employee info
+		Employees employee = employeesRepo.getEmployee(email);
+		
+		// Grab the password of the employee
+		String savedPassword = employee.getPassword();
+		
+		// Check the user input password against the saved password
+		if(password.equals(savedPassword)) {
+			
+			// Change b to true
+			b = true;
+			
+		}else {
+			// Log the error and throw a new exception
+			Exception e = new BusinessException("The password " + password + " does not match the recorded password " + savedPassword);
+			log.trace(e.getMessage());
+		}
+		
+		return b;
 	}
 
 	@Override
