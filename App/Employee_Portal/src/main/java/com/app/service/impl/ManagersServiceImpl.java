7f4f5756f1f17118b85a714033908531fb795cf2
Michael Pearson <mpearson2516@yahoo.com>
2021-02-26 08:50:50 -0800

    Finished ManagersServiceImpl

--- App/Employee_Portal/src/main/java/com/app/service/impl/ManagersServiceImpl.java
+++ App/Employee_Portal/src/main/java/com/app/service/impl/ManagersServiceImpl.java
@@ -2,16 +2,41 @@ package com.app.service.impl;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.app.exceptions.BusinessException;
+import com.app.exceptions.EmptyListException;
 import com.app.model.Managers;
+import com.app.repository.ManagersRepo;
+import com.app.repository.impl.ManagersRepoImpl;
 import com.app.service.ManagersService;
 
 public class ManagersServiceImpl implements ManagersService {
+	
+	// Instance of Repository layer implementation class
+	private ManagersRepo managersRepo = new ManagersRepoImpl();
+	
+	// Logger variable
+	Logger log = Logger.getLogger(ManagersServiceImpl.class);
 
 	@Override
 	public List<Managers> getManagers() throws BusinessException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// Initial list variable
+		List<Managers> allManagers = null;
+		
+		// Append the list with the data from the DB
+		try {
+			for(Managers manager : managersRepo.getManagers()) {
+				allManagers.add(manager);
+			}
+			
+			
+		} catch (EmptyListException e) {
+			log.trace(e.getMessage());
+			throw new BusinessException("Check connection to the DB");
+		}
+		return allManagers;
 	}
 
 }
