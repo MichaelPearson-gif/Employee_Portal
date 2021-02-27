8f608a16fb236854533cf8a0a1e0544b0f1ca71c
Michael Pearson <mpearson2516@yahoo.com>
2021-02-27 15:56:40 -0800

    2/7 request service methods done

--- App/Employee_Portal/src/main/java/com/app/service/impl/RequestsServiceImpl.java
+++ App/Employee_Portal/src/main/java/com/app/service/impl/RequestsServiceImpl.java
@@ -1,10 +1,12 @@
 package com.app.service.impl;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.log4j.Logger;
 
 import com.app.exceptions.BusinessException;
+import com.app.exceptions.EmptyListException;
 import com.app.model.Requests;
 import com.app.repository.RequestsRepo;
 import com.app.repository.impl.RequestsRepoImpl;
@@ -28,8 +30,32 @@ public class RequestsServiceImpl implements RequestsService {
 
 	@Override
 	public List<Requests> getPendingRequestsByEmail(String email) throws BusinessException {
-		// TODO Auto-generated method stub
-		return null;
+		
+		// Initial list object that will be returned
+		List<Requests> employeePendingRequests = new ArrayList<>();
+		
+		try {
+			
+			// Filtered list variable that will store all the requests
+			List<Requests> filteredRequests = requestsRepo.getRequests();
+			
+			// Filter the list by employee email
+			filteredRequests.removeIf((f) -> f.getEmployee().getEmail().equals(email));
+			// Filter by status of request to only pending requests
+			filteredRequests.removeIf((f) -> f.getStatus().equals("Pending"));
+			
+			// Append the employeePendingRequests with elements of the filtered list
+			for(Requests request : filteredRequests) {
+				employeePendingRequests.add(request);
+			}
+			
+		} catch (EmptyListException e) {
+			
+			log.trace(e.getMessage());
+			throw new BusinessException("Could not get list of requests. Check connection to the DB");
+		}
+		
+		return employeePendingRequests;
 	}
 
 	@Override
