e897caeada11588065f336c9ae05e18cccaa9b33
Michael Pearson <mpearson2516@yahoo.com>
2021-02-27 15:07:13 -0800

    1/7 request service methods done

--- App/Employee_Portal/src/main/java/com/app/service/impl/RequestsServiceImpl.java
+++ App/Employee_Portal/src/main/java/com/app/service/impl/RequestsServiceImpl.java
@@ -2,15 +2,27 @@ package com.app.service.impl;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
+
 import com.app.exceptions.BusinessException;
 import com.app.model.Requests;
+import com.app.repository.RequestsRepo;
+import com.app.repository.impl.RequestsRepoImpl;
 import com.app.service.RequestsService;
 
 public class RequestsServiceImpl implements RequestsService {
 
+	// Instance of the Repository layer implementation class
+	private RequestsRepo requestsRepo = new RequestsRepoImpl();
+	
+	// Logger variabel
+	Logger log = Logger.getLogger(RequestsServiceImpl.class);
+	
 	@Override
 	public void newRequest(Requests request) throws BusinessException {
-		// TODO Auto-generated method stub
+		
+		// Call the newRequest method from the repository layer
+		requestsRepo.newRequest(request);
 
 	}
 
